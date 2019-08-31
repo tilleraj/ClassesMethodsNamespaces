7606d1634e8e8d29583aff2dca12faff2e40489a
Andrew Tiller <tiller.andrew@gmail.com>
2019-08-31 10:54:28 -0500

    first commit. has .gitignore

---
+++ ClassesMethodsNamespaces/FavoriteThings/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesMethodsNamespaces.FavoriteThings
+{
+    class Book
+    {
+        readonly int _totalPages;
+        readonly string _title;
+        int _currentPage;
+        bool _completed;
+
+        public Book(int totalPages, string title)
+        {
+            _totalPages = totalPages;
+            _title = title;
+        }
+
+        public void Read(int pages)
+        {
+            if(_currentPage + pages > _totalPages)
+            {
+                _currentPage = _totalPages;
+                _completed = true;
+                Console.WriteLine($"You have finished {_title}");
+            }
+            else
+            {
+                _currentPage += pages;
+                Console.WriteLine($"You are currently on page {_currentPage} of {_title}");
+            }
+        }
+
+        public void Status()
+        {
+            if (!_completed)
+            {
+                Console.WriteLine($"You are currently on page {_currentPage} of {_title}");
+            }
+            else
+            {
+                int pagesRemaining = _totalPages - _currentPage;
+                Console.WriteLine($"You have {pagesRemaining} pages remaining in {_title}");
+            }
+        }
+    }
+}
---
+++ ClassesMethodsNamespaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassesMethodsNamespaces.FavoriteThings;
+using System;
+
+namespace ClassesMethodsNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the title of a book followed by a ',' and the number of pages in the book");
+            var input = Console.ReadLine();
+            string[] splitInput = input.Split(",");
+            var book1 = new Book(int.Parse(splitInput[1]), splitInput[0]);
+
+            var book2 = new Book(123, "Book2: The Title");
+
+            book1.Read(20);
+            book1.Status();
+
+            book2.Read(200);
+            book2.Status();
+
+        }
+    }
+}
